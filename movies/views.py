9d542e932d07f49ade3e0e31b8c9bc33798e1437
aananda-giri <aanandaprashadgiri@gmail.com>
2023-04-11 00:06:29 +0545

    debug

--- movies/views.py
+++ movies/views.py
@@ -6,8 +6,7 @@ from django.core import serializers
 from urllib3 import HTTPResponse
 import json, os
 # json.  
-# API_KEY = os.environ.get('TMDB_API_KEY');
-API_KEY = os.environ.get('TMDB_API_KEY')
+API_KEY = os.environ.get('TMDB_API_KEY');
 BASE_URL = 'https://api.themoviedb.org/3';
 API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
 IMG_URL = 'https://image.tmdb.org/t/p/w500';
@@ -17,13 +16,15 @@ searchURL = BASE_URL + '/search/movie?' + API_KEY;
 def home(request):
     # return render(request,'movies/home.html')
     # https://api.themoviedb.org/3/tv/top_rated?api_key=<<TMDB_API_KEY>>&language=en-US&page=1?adult=true
-    trending_all = requests.get(BASE_URL + '/trending/all/week?' + API_KEY).json()['results']
+    trending_all = requests.get(BASE_URL + '/trending/all/week?api_key=' + API_KEY).json()['results']
+    print(f'trending_all: \n\n {trending_all}')
+    # trending_all = trending_all['results']
     # print(trending_all)
-    popular_movie = requests.get(BASE_URL + '/movie/popular?' + API_KEY + '&language=en-US&page=1').json()['results']
-    popular_series = requests.get(BASE_URL + '/tv/popular?' + API_KEY + '&language=en-US&page=1').json()['results']
-    latest_movie = requests.get(BASE_URL + '/movie/now_playing?' + API_KEY + '&language=en-US&page=1').json()['results']
-    latest_series = requests.get(BASE_URL + '/tv/on_the_air?' + API_KEY + '&language=en-US&page=1').json()['results']
-    upcomming = requests.get(BASE_URL + '/movie/upcoming?' + API_KEY + '&language=en-US&page=1').json()['results']
+    popular_movie = requests.get(BASE_URL + '/movie/popular?api_key=' + API_KEY + '&language=en-US&page=1').json()['results']
+    popular_series = requests.get(BASE_URL + '/tv/popular?api_key=' + API_KEY + '&language=en-US&page=1').json()['results']
+    latest_movie = requests.get(BASE_URL + '/movie/now_playing?api_key=' + API_KEY + '&language=en-US&page=1').json()['results']
+    latest_series = requests.get(BASE_URL + '/tv/on_the_air?api_key=' + API_KEY + '&language=en-US&page=1').json()['results']
+    upcomming = requests.get(BASE_URL + '/movie/upcoming?api_key=' + API_KEY + '&language=en-US&page=1').json()['results']
 
     context = {'categories':['Trending', 'popular_movie', 'popular_series', 'latest_movie', 'upcomming'], 'values':{'Trending': trending_all, 'Popular Movies': popular_movie, 'Popular Series' : popular_series, 'Latest Movies': latest_movie, 'Latest Series': latest_series, 'Upcomming' : upcomming}}
     return render(request, 'movies/home.html', context)
@@ -39,7 +40,7 @@ def search(request):
         search_term_parsed = urllib.parse.quote(search_term)
         print('\n\nsearch:',search_term_parsed)
         # t/p/w500/pHkKbIRoCe7zIFvqan9LFSaQAde.jpg
-        search_results = requests.get(f'https://api.themoviedb.org/3/search/multi?api_key=<<TMDB_API_KEY>>&language=en-US&page=1&include_adult=true&query={search_term}').json()['results']
+        search_results = requests.get(f'https://api.themoviedb.org/3/search/multi?api_key={API_KEY}&language=en-US&page=1&include_adult=true&query={search_term}').json()['results']
         # search_results = {'categories':['SEARCH : ' + str(search_term)], 'values':{'SEARCH {search_term} ' : search_results}}
         search_results = {'categories':[f'SEARCH <Movie> : {urllib.parse.unquote(search_term_parsed)}', f'SEARCH <MOVIE> : {urllib.parse.unquote(search_term_parsed)}'], 'values':{f'SEARCH : {urllib.parse.unquote(search_term_parsed)}' : search_results}, 'search_term' : search_term}
         # print('search_results',search_results)
@@ -49,7 +50,7 @@ def search(request):
         genre_parsed = urllib.parse.quote(genre)
         # genre_title = urllib.parse.quote(genre_title)
         # t/p/w500/pHkKbIRoCe7zIFvqan9LFSaQAde.jpg
-        search_results = requests.get(f'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&{API_KEY}&with_genres={genre_parsed}').json()['results']
+        search_results = requests.get(f'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key={API_KEY}&with_genres={genre_parsed}').json()['results']
         # print(f'\n\n genre: {genre_parsed}\n genre_results', search_results)
 
 
@@ -87,18 +88,18 @@ def series(request):
         
         
     # 'https://api.themoviedb.org/3/tv/60573/season/1?api_key=<<TMDB_API_KEY>>&language=en-US'
-    season_data = requests.get(f'https://api.themoviedb.org/3/tv/{tmdb_id}/season/{season}?api_key=<<TMDB_API_KEY>>&language=en-US')
+    season_data = requests.get(f'https://api.themoviedb.org/3/tv/{tmdb_id}/season/{season}?api_key={API_KEY}&language=en-US')
     print(season_data.json())
     
     # for similar series
     # https://api.themoviedb.org/3/tv/60573/similar?api_key=<<TMDB_API_KEY>>&language=en-US&page=1
-    similar = requests.get(f'https://api.themoviedb.org/3/tv/{tmdb_id}/similar?api_key=<<TMDB_API_KEY>>&language=en-US&page=1').json()
+    similar = requests.get(f'https://api.themoviedb.org/3/tv/{tmdb_id}/similar?api_key={API_KEY}&language=en-US&page=1').json()
     
     # https://api.themoviedb.org/3/tv/60573?api_key=<<TMDB_API_KEY>>&language=en-US
     # https://api.themoviedb.org/3/tv/60573/?season=6?episode=1/?api_key=<<TMDB_API_KEY>>&language=en-US
     # print(f'series_url: https://api.themoviedb.org/3/tv/{tmdb_id}?api_key=<<TMDB_API_KEY>>&language=en-US')
     # print(f'\n movie_url: {movie_url}')
-    series_data = requests.get(f'https://api.themoviedb.org/3/tv/{tmdb_id}?api_key=<<TMDB_API_KEY>>&language=en-US').json()
+    series_data = requests.get(f'https://api.themoviedb.org/3/tv/{tmdb_id}?api_key={API_KEY}&language=en-US').json()
     number_of_seasons = series_data['number_of_seasons']
 
     context = {'tmdb_id': tmdb_id, 'what': 'series', 'season_number': season, 'episode_number': int(episode), 'movie_url': movie_url, 'season_data': season_data.json(), 'number_of_seasons': number_of_seasons, 'stream' : 'series', 'source_index': source, 'series_data':series_data, 'similar': similar}
@@ -133,11 +134,11 @@ def movie(request):
 
 
         # https://api.themoviedb.org/3/movie/1726?api_key=<<TMDB_API_KEY>>&language=en-US
-        movie_data = requests.get(f'https://api.themoviedb.org/3/movie/{tmdb_id}?api_key=<<TMDB_API_KEY>>&language=en-US').json()
+        movie_data = requests.get(f'https://api.themoviedb.org/3/movie/{tmdb_id}?api_key={API_KEY}&language=en-US').json()
         # print('req_url: ', 'https://api.themoviedb.org/3/movie/{tmdb_id}?api_key=<<TMDB_API_KEY>>&language=en-US')
 
         # https://api.themoviedb.org/3/movie/1726/similar?api_key=<<TMDB_API_KEY>>&language=en-US&page=1
-        similar = requests.get(f'https://api.themoviedb.org/3/movie/{tmdb_id}/similar?api_key=<<TMDB_API_KEY>>&language=en-US&page=1').json()
+        similar = requests.get(f'https://api.themoviedb.org/3/movie/{tmdb_id}/similar?api_key={API_KEY}&language=en-US&page=1').json()
 
         # get actual movie info by tmdb id
         # movie_data = 
@@ -155,9 +156,9 @@ def trailer(request):
         tmdb_id = request.GET.get('tmdb_id', None)
         what = request.GET.get('what', None)    # movie or series
         if what == 'movie':
-            res = requests.get(f'https://api.themoviedb.org/3/movie/{tmdb_id}/videos?api_key=<<TMDB_API_KEY>>').json()
+            res = requests.get(f'https://api.themoviedb.org/3/movie/{tmdb_id}/videos?api_key={API_KEY}').json()
         else:
-            res = requests.get(f'https://api.themoviedb.org/3/tv/{tmdb_id}/videos?api_key=<<TMDB_API_KEY>>').json()
+            res = requests.get(f'https://api.themoviedb.org/3/tv/{tmdb_id}/videos?api_key={API_KEY}').json()
         print(f'\n\ntmdb_id : {tmdb_id},  res: {res}')
         return JsonResponse(res)
     else:
