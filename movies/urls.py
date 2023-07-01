e843c130428cbbbcd258c90966c3c368f3476772
aananda-giri <aanandaprashadgiri@gmail.com>
2023-07-01 19:29:39 +0545

    minor debug

--- movies/urls.py
+++ movies/urls.py
@@ -4,10 +4,10 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('search/', search, name='search'),
-    path('movies/', list_movies, name='movies'),    # list movies page
-    path('series/', list_series, name='series'),    # list series pageqq
     path('movie/', movie, name='movie'),    # watch movie page
     path('series/', series, name='series'), # watch series page
+    path('movies/', list_movies, name='movies'),    # list movies page
+    path('series/', list_series, name='series'),    # list series page
     path('trailer/', trailer, name='trailer'),
     path('country/', country, name='country'),
     path('genre/', genre, name='genre'),
